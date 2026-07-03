c0216f7a3a0ba994d3f0a906cee42de9cffe19e0
spirit1zero-ss <710041617@qq.com>
2026-07-03 13:27:36 +0800

    chore: remove unused uni app surfaces

--- src/CRMEB/CRMEB-master/template/uni-app/api/education.js
+++
@@ -1,5 +0,0 @@
-import request from "@/utils/request.js";
-
-export function submitAssessmentRecord(data) {
-	return request.post("education/assessment_records", data);
-}
--- src/CRMEB/CRMEB-master/template/uni-app/scripts/node-compat.cjs
+++
@@ -1,7 +0,0 @@
-const util = require('util');
-
-if (typeof util.isRegExp !== 'function') {
-  util.isRegExp = function isRegExp(value) {
-    return Object.prototype.toString.call(value) === '[object RegExp]';
-  };
-}
