d0e2c23900e143074bb505518e20ff93896925bb
spirit1zero-ss <710041617@qq.com>
2026-07-13 13:30:13 +0800

    deploy: add WeChat CloudRun container

--- src/CRMEB/CRMEB-master/crmeb/config/session.php
+++ src/CRMEB/CRMEB-master/crmeb/config/session.php
@@ -22,13 +22,16 @@
 // | 会话设置
 // +----------------------------------------------------------------------
 
+use think\facade\Env;
+
 return [
     // session name
     'name'           => '',
     // SESSION_ID的提交变量,解决flash上传跨域
     'var_session_id' => '',
     // 驱动方式 支持file redis memcache memcached
-    'type'           => 'file',
+    'type'           => Env::get('session.type', 'file'),
+    'store'          => Env::get('session.store', ''),
     // 过期时间
     'expire'         => 10800,
     // 前缀
